a41957354006b611f6f685dd017881050ef3059c
Blackcito <137465887+Blackcito@users.noreply.github.com>
2023-10-06 00:07:27 -0300

    Update practico2.R

--- practico2.R
+++ practico2.R
@@ -5,7 +5,7 @@ library(Hmisc)
 library(TTR)
 library(scales)
 
-data <- read_csv("C:\\Users\\alder\\Downloads\\base_de_datos\\emails.csv")
+#data <- read_csv("C:\\Users\\alder\\Downloads\\base_de_datos\\emails.csv")
 # Establece una semilla aleatoria para que la división sea reproducible
 set.seed(123)
 
@@ -35,25 +35,86 @@ prueba <- prueba[, -1]
 total_spam <- sum(entrenamiento$Prediction == 1)
 total_ham <- n_entrenamiento - total_spam
 
-# Crea una matriz para almacenar las probabilidades
-probabilidades <- matrix(0, nrow = ncol(entrenamiento), ncol = 2)
 
-# Itera a través de cada palabra (columna) en el conjunto de entrenamiento
-for (i in 2:ncol(entrenamiento)) {  # Comenzamos desde la columna 2 (la primera columna es la etiqueta)
-  # Contar cuántas veces aparece la palabra en correos spam y ham
-  word_count_spam <- sum(entrenamiento[entrenamiento$Prediction == 1, i])
-  word_count_ham <- sum(entrenamiento[entrenamiento$Prediction == 0, i])
+
+# Valor constante 'm'
+m <- 30000  # Puedes ajustar el valor de 'm' según tus necesidades
+
+# Crear un dataframe vacío para almacenar las probabilidades
+probabilidades_df <- data.frame(Palabra = character(0), Probabilidad_1 = numeric(0), Probabilidad_0 = numeric(0))
+
+# Iterar a través de las columnas de Bag of Words, excluyendo la primera y la última columna
+for (i in 1:(ncol(entrenamiento) - 1)) {  # Excluyendo la primera y la última columna
   
-  # Calcular las probabilidades con suavizado de Laplace
-  prob_spam <- (word_count_spam + 1) / (total_spam + 2)  # Agregamos 1 al numerador y 2 al denominador
-  prob_ham <- (word_count_ham + 1) / (total_ham + 2)
+  # Extraer la palabra actual
+  palabra_actual <- names(entrenamiento)[i]
   
-  # Almacenar las probabilidades en la matriz
-  probabilidades[i - 1, 1] <- prob_spam  # Restamos 1 para evitar la primera columna
-  probabilidades[i - 1, 2] <- prob_ham
+  # Contar(Wi ∩ Clase) + 1 para 1
+  Conteo_Wi_Clase_1 <- sum(entrenamiento[[i]][entrenamiento$Prediction == 1]) + 1
+  
+  # Contar(Wi ∩ Clase) + 1 para 0
+  Conteo_Wi_Clase_0 <- sum(entrenamiento[[i]][entrenamiento$Prediction == 0]) + 1
+  
+  # Contar(Clase) + m para 1
+  Conteo_Clase_1 <- sum(entrenamiento$Prediction == 1) + m
+  
+  # Contar(Clase) + m para 0
+  Conteo_Clase_0 <- sum(entrenamiento$Prediction == 0) + m
+  
+  # Calcular la probabilidad para 1
+  Probabilidad_Spam <- Conteo_Wi_Clase_1 / Conteo_Clase_1
+  
+  # Calcular la probabilidad para 0
+  Probabilidad_Ham <- Conteo_Wi_Clase_0 / Conteo_Clase_0
+  
+  if (Probabilidad_Spam > Probabilidad_Ham){
+    binario_vector = 1
+  } 
+  else{
+    binario_vector = 0
+  }
+  
+  # Agregar los resultados al dataframe
+  nueva_fila <- data.frame(Palabra = palabra_actual, Probabilidad_1 = Probabilidad_Spam, Probabilidad_0 = Probabilidad_Ham, binario = binario_vector)
+  probabilidades_df <- rbind(probabilidades_df , nueva_fila)
+}
+head(probabilidades_df)
+
+
+
+
+# Crear un dataframe vacío para almacenar los resultados
+resultados_df <- data.frame(
+  Palabra = character(0),
+  Wi_Spam = numeric(0),
+  Wi_Ham = numeric(0)
+)
+
+# Iterar a través de las filas del dataframe probabilidades_df
+for (j in 1:nrow(probabilidades_df)) {
+  
+  # Acceder a los valores necesarios de probabilidades_df
+  binario <- probabilidades_df$binario[j]
+  Probabilidad_1 <- probabilidades_df$Probabilidad_1[j]
+  Probabilidad_0 <- probabilidades_df$Probabilidad_0[j]
+  
+  # Calcular los valores Wi_Spam y Wi_Ham
+  wi_spam <- binario * log(Probabilidad_1) + (1 - binario) * log(1 - Probabilidad_1)
+  wi_ham <- binario * log(Probabilidad_0) + (1 - binario) * log(1 - Probabilidad_0)
+  
+  # Agregar los resultados al dataframe resultados_df
+  nueva_fila <- data.frame(Palabra = probabilidades_df$Palabra[j], Wi_Spam = wi_spam, Wi_Ham = wi_ham)
+  resultados_df <- rbind(resultados_df, nueva_fila)
 }
 
-# Convierte la matriz de probabilidades en un data frame con nombres de columna
-probabilidades_df <- data.frame(Word = colnames(entrenamiento), Prob_Spam = probabilidades[, 1], Prob_Ham = probabilidades[, 2])
-# Imprime las primeras filas del data frame de probabilidades
-head(probabilidades_df)
+for(i in nrow(1:resultados_df)){
+  
+  resultado <- log1pexp(resultados_df$Wi_Spam[i] + log_P_Spam, resultados_df$wi_ham[i] + log_P_Ham)
+  
+}
+
+
+
+
+
+
